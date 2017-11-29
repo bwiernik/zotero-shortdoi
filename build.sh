fd9d7c24c35be62cdce298a17fc88ca061ae95a3
bwiernik <github@wiernik.org>
2017-11-29 18:14:41 +0100

    Update build and update scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 version=1.0.0
-rm builds/zotero-shortdoi-${version}-fx.xpi
+rm builds/zotero-shortdoi-${version}.xpi
 zip -r builds/zotero-shortdoi-${version}-fx.xpi chrome/* chrome.manifest install.rdf options.xul
