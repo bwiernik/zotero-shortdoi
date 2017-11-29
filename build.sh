7151986d4508f9e0b017c02f5bf78b10017eae0f
bwiernik <github@wiernik.org>
2017-11-29 18:29:13 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 version=1.0.1
 rm builds/zotero-shortdoi-${version}.xpi
-zip -r builds/zotero-shortdoi-${version}-fx.xpi chrome/* chrome.manifest install.rdf options.xul
+zip -r builds/zotero-shortdoi-${version}.xpi chrome/* chrome.manifest install.rdf options.xul
